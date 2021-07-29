9d0f46649adfca643811bf231412b7b5fa71b65f
lucianoliveirasouza <lucianoliveirasouza@gmail.com>
2021-07-29 17:31:56 -0300

    RestTemplate

--- hr-payroll/src/main/java/luciano/frabrica/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/luciano/frabrica/hrpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package luciano.frabrica.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import luciano.frabrica.hrpayroll.entities.Payment;
+import luciano.frabrica.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(Long id, int days) {
-		return new Payment("Bob",200.0,days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", id.toString());
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(),days);
 	}
 }
